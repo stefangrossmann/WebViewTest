487e0c8448a0f477b4eed26cd5c6d15776471150
Stefan <stefan.grossman1@gmx.de>
2020-11-14 20:00:10 +0100

    Add cookie redirect handling

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # WebViewTest
-Just playing with Swift
+
+Just playing with Swift ...
+
+[See: WKWebView redirect with cookies] (https://www.novemberfive.co/blog/wkwebview-redirect-with-cookies)
--- WebViewTest/WebView.swift
+++ WebViewTest/WebView.swift
@@ -1,33 +1,11 @@
-//
-//  GoswWebView.swift
-//  WebViewTest
-//
-//  Created by Stefan Großmann on 14.11.20.
-//
-
 import SwiftUI
 import WebKit
-
-class WKRedirectWebView : WKWebView {}
-
-extension WKRedirectWebView : WKUIDelegate {
-    public func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: (WKNavigationActionPolicy) -> Swift.Void) {
-            if(navigationAction.navigationType == .other) {
-                if navigationAction.request.url != nil {
-                    self.load(navigationAction.request)
-                }
-                decisionHandler(.cancel)
-                return
-            }
-            decisionHandler(.allow)
-        }
-}
 
 struct WebView : UIViewRepresentable {
     let request: URLRequest
     
     func makeUIView(context: Context) -> WKWebView  {
-        return WKRedirectWebView()
+        return WKCookieWebView()
     }
     
     func updateUIView(_ uiView: WKWebView, context: Context) {
